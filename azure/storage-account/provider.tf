8baa3a3c68f72a3e08099918b5d7b74539481810
Carlos Tadeu <carlinhos.slz@hotmail.com>
2024-09-29 19:16:04 -0300

    new branch

--- azure/storage-account/provider.tf
+++ azure/storage-account/provider.tf
@@ -8,5 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = "your-subscription-id"
+  client_id       = "your-client-id"
+  client_secret   = "your-client-secret"
+  tenant_id       = "your-tenant-id"
   features {}
 }
